3808687c44be2bc3f52a66f63f910fcf6b46e7bd
Obaro Awowede <akpobaroawowede@gmail.com>
2025-05-22 14:01:33 +0100

    third correction

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-monthly_savings = income - expenses
+monthly_savings = monthly_income - monthly_expenses
 interest = 0.05
 yearly_savings = monthly_savings * 12 + (monthly_savings * 12 * interest)
 
