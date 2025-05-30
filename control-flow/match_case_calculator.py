a67ea867bfe8b505bde2a2658174139e16988859
Obaro Awowede <akpobaroawowede@gmail.com>
2025-05-30 18:21:39 +0100

    pushed task 1

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the first number:"))
+
+ask = str(input("Choose the operation (+, -, *, /):"))
+
+if ask == "+":
+    result = num1 + num2
+    print("the result is ", result)
+if ask == "-":
+    result = num1 - num2
+    print("the result is ", result)
+if ask == "/":
+    result = num1 / num2
+    print("the result is ", result)
+if ask == "*":
+    result = num1 * num2
+    print("the result is ", result)
