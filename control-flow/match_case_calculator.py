613e37f1683880a8b6014a34a389df1ced36909c
Obaro Awowede <akpobaroawowede@gmail.com>
2025-05-30 18:22:55 +0100

    hmm

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,5 +1,5 @@
 num1 = int(input("Enter the first number:"))
-num2 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
 
 ask = str(input("Choose the operation (+, -, *, /):"))
 
