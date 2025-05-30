e797158b15a876cfee5f80fbfbea66d47e25a77e
Obaro Awowede <akpobaroawowede@gmail.com>
2025-05-30 18:24:26 +0100

    used checker's syntax

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,17 +1,17 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
 
-ask = str(input("Choose the operation (+, -, *, /):"))
+operation = str(input("Choose the operation (+, -, *, /):"))
 
-if ask == "+":
+if operation == "+":
     result = num1 + num2
     print("the result is ", result)
-if ask == "-":
+if operation == "-":
     result = num1 - num2
     print("the result is ", result)
-if ask == "/":
+if operation == "/":
     result = num1 / num2
     print("the result is ", result)
-if ask == "*":
+if operation == "*":
     result = num1 * num2
     print("the result is ", result)
