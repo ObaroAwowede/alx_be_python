e0b84d245020e13abad979a55efc368b79651299
Obaro Awowede <akpobaroawowede@gmail.com>
2025-06-12 15:14:22 +0100

    done task 3

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,26 @@
+global FAHRENHEIT_TO_CELSIUS_FACTOR
+global CELSIUS_TO_FAHRENHEIT_FACTOR
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+
+def convert_to_celsius(fahrenheit):
+    temp = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(fahrenheit,"°F is ",temp,"°C" )
+    
+
+
+def convert_to_fahrenheit(celsius):
+    temp = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(celsius,"°C is ",temp,"°F" )
+    
+
+
+temp = float(input("Enter the temperature to convert: "))
+choice = str(input("Is this temperature in Celsius or Fahrenheit? (C/F): "))
+
+if choice == "C":
+    convert_to_fahrenheit(temp)
+elif choice == "F":
+    convert_to_celsius(temp)
+else:
+    "Input a valid option"
