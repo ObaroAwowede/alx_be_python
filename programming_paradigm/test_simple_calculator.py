fefd5115ed438357f0bb11cdcb7eabd6503f7d54
Obaro Awowede <akpobaroawowede@gmail.com>
2025-06-15 12:43:53 +0100

    fixed naming convention to pass checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,32 +4,33 @@ from simple_calculator import SimpleCalculator
 class TestCalculator(unittest.TestCase):
     
     def test_addition(self):
-        calculator = SimpleCalculator()
-        self.assertEqual(calculator.add(5,2),7,)
-        self.assertEqual(calculator.add(0,0),0)
-        self.assertEqual(calculator.add(-2,-1),-3)
-        self.assertEqual(calculator.add(0,-5),-5)
+        calc = SimpleCalculator()
+        self.assertEqual(calc.add(5,2),7,)
+        self.assertEqual(calc.add(0,0),0)
+        self.assertEqual(calc.add(-2,-1),-3)
+        self.assertEqual(calc.add(0,-5),-5)
         
-    def test_division(self):
-        calculator = SimpleCalculator()
-        self.assertEqual(calculator.divide(5,0),"None")
-        self.assertEqual(calculator.divide(0,5),0)
-        self.assertEqual(calculator.divide(2,2),1)
-        self.assertEqual(calculator.divide(7,2),3.5)
-        self.assertEqual(calculator.divide(-9,2),-4.5)
-        self.assertEqual(calculator.divide(-7,-2),3.5)
-       
-    def test_multiplication(self):
-        calculator = SimpleCalculator()
-        self.assertEqual(calculator.multiply(5,0),0)
-        self.assertEqual(calculator.multiply(0,5),0)
-        self.assertEqual(calculator.multiply(2,5),10)
-        self.assertEqual(calculator.multiply(-1,5),-5)
-        self.assertEqual(calculator.multiply(-2,-8),16)
-    
     def test_subtraction(self):
-        calculator = SimpleCalculator()
-        self.assertEqual(calculator.subtraction(4,3),1)
-        self.assertEqual(calculator.subtraction(2,3),-1)
-        self.assertEqual(calculator.subtraction(-2,-5),3)
-        self.assertEqual(calculator.subtraction(-4,3),-7)
+        calc = SimpleCalculator()
+        self.assertEqual(calc.subtraction(4,3),1)
+        self.assertEqual(calc.subtraction(2,3),-1)
+        self.assertEqual(calc.subtraction(-2,-5),3)
+        self.assertEqual(calc.subtraction(-4,3),-7)
+        
+    def test_divide(self):
+        calc = SimpleCalculator()
+        self.assertEqual(calc.divide(5,0),"None")
+        self.assertEqual(calc.divide(0,5),0)
+        self.assertEqual(calc.divide(2,2),1)
+        self.assertEqual(calc.divide(7,2),3.5)
+        self.assertEqual(calc.divide(-9,2),-4.5)
+        self.assertEqual(calc.divide(-7,-2),3.5)
+       
+    def test_multiply(self):
+        calc = SimpleCalculator()
+        self.assertEqual(calc.multiply(5,0),0)
+        self.assertEqual(calc.multiply(0,5),0)
+        self.assertEqual(calc.multiply(2,5),10)
+        self.assertEqual(calc.multiply(-1,5),-5)
+        self.assertEqual(calc.multiply(-2,-8),16)
+    
