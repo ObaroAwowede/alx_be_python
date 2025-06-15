28796923d6b3e79e75c2443201ed2464468a8612
Obaro Awowede <akpobaroawowede@gmail.com>
2025-06-15 12:50:55 +0100

    fixed syntax to pass checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,35 +2,34 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator() 
     
     def test_addition(self):
-        calc = SimpleCalculator()
-        self.assertEqual(calc.add(5,2),7,)
-        self.assertEqual(calc.add(0,0),0)
-        self.assertEqual(calc.add(-2,-1),-3)
-        self.assertEqual(calc.add(0,-5),-5)
+        self.assertEqual(self.calc.add(5,2),7,)
+        self.assertEqual(self.calc.add(0,0),0)
+        self.assertEqual(self.calc.add(-2,-1),-3)
+        self.assertEqual(self.calc.add(0,-5),-5)
         
     def test_subtraction(self):
         calc = SimpleCalculator()
-        self.assertEqual(calc.subtraction(4,3),1)
-        self.assertEqual(calc.subtraction(2,3),-1)
-        self.assertEqual(calc.subtraction(-2,-5),3)
-        self.assertEqual(calc.subtraction(-4,3),-7)
+        self.assertEqual(self.calc.subtraction(4,3),1)
+        self.assertEqual(self.calc.subtraction(2,3),-1)
+        self.assertEqual(self.calc.subtraction(-2,-5),3)
+        self.assertEqual(self.calc.subtraction(-4,3),-7)
         
     def test_divide(self):
-        calc = SimpleCalculator()
-        self.assertEqual(calc.divide(5,0),"None")
-        self.assertEqual(calc.divide(0,5),0)
-        self.assertEqual(calc.divide(2,2),1)
-        self.assertEqual(calc.divide(7,2),3.5)
-        self.assertEqual(calc.divide(-9,2),-4.5)
-        self.assertEqual(calc.divide(-7,-2),3.5)
+        self.assertEqual(self.calc.divide(5,0),"None")
+        self.assertEqual(self.calc.divide(0,5),0)
+        self.assertEqual(self.calc.divide(2,2),1)
+        self.assertEqual(self.calc.divide(7,2),3.5)
+        self.assertEqual(self.calc.divide(-9,2),-4.5)
+        self.assertEqual(self.calc.divide(-7,-2),3.5)
        
     def test_multiply(self):
-        calc = SimpleCalculator()
-        self.assertEqual(calc.multiply(5,0),0)
-        self.assertEqual(calc.multiply(0,5),0)
-        self.assertEqual(calc.multiply(2,5),10)
-        self.assertEqual(calc.multiply(-1,5),-5)
-        self.assertEqual(calc.multiply(-2,-8),16)
+        self.assertEqual(self.calc.multiply(5,0),0)
+        self.assertEqual(self.calc.multiply(0,5),0)
+        self.assertEqual(self.calc.multiply(2,5),10)
+        self.assertEqual(self.calc.multiply(-1,5),-5)
+        self.assertEqual(self.calc.multiply(-2,-8),16)
     
