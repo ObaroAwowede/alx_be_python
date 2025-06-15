294ec708d79e66e25dab1878b966d6debb64c32f
Obaro Awowede <akpobaroawowede@gmail.com>
2025-06-15 12:52:14 +0100

    fixed function name to pass checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -18,7 +18,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtraction(-2,-5),3)
         self.assertEqual(self.calc.subtraction(-4,3),-7)
         
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(5,0),"None")
         self.assertEqual(self.calc.divide(0,5),0)
         self.assertEqual(self.calc.divide(2,2),1)
@@ -26,7 +26,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.divide(-9,2),-4.5)
         self.assertEqual(self.calc.divide(-7,-2),3.5)
        
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(5,0),0)
         self.assertEqual(self.calc.multiply(0,5),0)
         self.assertEqual(self.calc.multiply(2,5),10)
